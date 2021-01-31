8762bcdbbf4ba79066f2126d530c91b57450ebb2
Nagaaswin <Nagaaswin775@gmail.com>
2021-01-31 18:17:55 +0530

    Added features

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const description = document.querySelector('p:last-of-type');
 const form = document.querySelector('#copyDetails');
 const stopCopy = document.querySelector('#stopCopy');
-const url =  'https://general-arc-java.herokuapp.com' //  'http://localhost:5000'; // 
+const url = 'https://general-arc-java.herokuapp.com'; //'http://localhost:5000'; //
 let stompClient = null;
+let flag = false;
 
 stompClient = new window.StompJs.Client({
   webSocketFactory: function () {
@@ -40,30 +41,32 @@ function frameHandler(frame) {
   console.log('Connected: ' + frame);
   sendMessage();
   stompClient.subscribe('/topic/messages', function (message) {
-    //console.log(message);
     var msg = JSON.parse(message.body);
     if (msg.isalive == 'true') {
+      flag = false;
       showMessage(msg);
       sendMessage();
     } else {
+      flag = true;
       return;
     }
   });
 }
 
 function showMessage(message) {
-  //console.log(message);
-  const content = message.content;
-  description.insertAdjacentHTML('afterend', `<p>${content}</p>`);
+  const contents = message.content;
+  for (const content of contents) {
+    description.insertAdjacentHTML('afterend', `<p>${content}</p>`);
+  }
 }
 
 axios.get(`${url}/setUpRclone`);
 
-//let time = setInterval(statusOfTheTerminal, 2000);
-
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  copyRequest();
+  if (flag) {
+    copyRequest();
+  }
 });
 
 stopCopy.addEventListener('submit', (event) => {
@@ -74,7 +77,6 @@ stopCopy.addEventListener('submit', (event) => {
 async function stopCopyHandler() {
   try {
     axios.get(`${url}/stopCopying`);
-    //clearInterval(time);
   } catch (err) {
     console.log(err);
   }
@@ -85,24 +87,7 @@ async function copyRequest() {
     const fd = new FormData(form);
     axios.post(`${url}/change`, fd);
     setTimeout(sendMessage, 2000);
-    //time = setInterval(statusOfTheTerminal, 2000);
-    // statusOfTheTerminal();
   } catch (err) {
     console.log(err);
   }
 }
-
-// async function statusOfTheTerminal() {
-//   try {
-//     const response = await axios.get(`${url}/status`);
-//     if (response.data.isAlive == 'false') {
-//       clearInterval(time);
-//       return;
-//     } else {
-//       const content = response.data.Content;
-//       description.insertAdjacentHTML('afterend', `<p>${content}</p>`);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
