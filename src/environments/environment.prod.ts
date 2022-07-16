417b6bb2b09b8f4a400ada680396f11b53e3746f
Nagaaswin S <nagaaswin775@gmail.com>
2022-07-16 11:17:11 +0530

    updated to new url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocket: 'wss://cloud-op-app.herokuapp.com/cloud-op/websocket',
-  cloudOpBaseUrl: 'https://cloud-op-app.herokuapp.com',
+  websocket: 'wss://cloud-operation.herokuapp.com/cloud-op/websocket',
+  cloudOpBaseUrl: 'https://cloud-operation.herokuapp.com',
 };
