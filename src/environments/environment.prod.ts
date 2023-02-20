3f3581e7c30a62ef336c0b80e6cea624b9426440
Nagaaswin S <nagaaswin.s@m2pfintech.com>
2023-02-20 10:27:47 +0530

    updated port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocket: 'ws://localhost:5000/cloud-op/websocket',
-  cloudOpBaseUrl: 'http://localhost:5000',
+  websocket: 'ws://localhost:5050/cloud-op/websocket',
+  cloudOpBaseUrl: 'http://localhost:5050',
 };
